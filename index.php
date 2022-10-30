ed13ec8a4010053e9987e0076c7d70070820bcc0
PetruTH <95080404+PetruTH@users.noreply.github.com>
2022-10-30 13:19:03 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,17 @@
+
+ <?php
+echo '<p>Am ales sa dezvolt o aplicatie care sa gestioneze activitatile din cadrul unui spital. <br>
+    Astfel, in cadrul acestei aplicatii va exista un meniu de tip CRUD pentru utilizatori (prin intermediul <br>
+    caruia sa isi creeze o noua programare, sa o actualizeze sau sa o anuleze), precum si unul pentru <br>
+    administratori (prin care sa gestioneze partea tehnica, de asemenea modifice actiunile utilizatorilor<br>
+    in anumite circumstante). Baza de date din spatele acestei aplicatii web va contine sectiile impreuna<br>
+    cu medicii spitalului, pacientii care au fost internati in ultimele 180 de zile sau sunt internati<br>
+    in prezent, aparatura medicala disponibila precum si saloanele spitalului.</p>';
+
+echo 'Interfata grafica a acestei aplicatii web va fi realizata cu ajutorul elementelor de HTML si CSS<br>
+    pentru a oferi o experienta user-friendly. Baza de date care va fi legata de aceasta aplicatie web,<br>
+    va fi realizata in MySQL, pentru a gestiona diferitele cereri ale utilizatorilor, precum si alte<br>
+    detalii tehnice ce doresc a fi modificate de catre administratorii aplicatiei web. Arhitectura aplicatiei va arata astfel: '; 
+
+echo '<img src="arh.png" alt="arhitectura">';
+?> 
